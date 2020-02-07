00e651e90fd2b06df7884db6c066c688db0ca26d
KasperPtak <60694854+KasperPtak@users.noreply.github.com>
2020-02-07 10:15:06 +0100

    Initial commit

---
+++ assets/js/Slider.js
@@ -0,0 +1,25 @@
+/* ----------------------------
+/*  Name: StrandSite
+    Author: KasperPtak
+    Version: 1
+/* -------------------------- */
+
+var slideIndex = 0;
+ShowSlides();
+
+function ShowSlides() {
+    var i;
+    var slides = document.getElementsByClassName("carousel-item");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(ShowSlides, 2000); // Change image every 2 seconds
+  }
